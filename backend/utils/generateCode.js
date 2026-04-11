fd2b821ce04cacc64481b7625aab518bb2c93066
shubh-2601s <10221shubham.s@gmail.com>
2026-04-11 15:35:44 +0530

    Fix ERR_REQUIRE_ESM by replacing nanoid with crypto

--- backend/utils/generateCode.js
+++ backend/utils/generateCode.js
@@ -1,11 +1,11 @@
-const { nanoid } = require('nanoid');
+const crypto = require('crypto');
 
 /**
  * Generates a short unique quiz code (8 uppercase alphanumeric chars)
  * e.g. "A3BX92PQ"
  */
 const generateCode = () => {
-  return nanoid(8).toUpperCase();
+  return crypto.randomBytes(4).toString('hex').toUpperCase();
 };
 
 module.exports = { generateCode };
